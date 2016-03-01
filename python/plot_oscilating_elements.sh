2a2b941c5a3c054dcb8575336df8f1ac1891eaf3
Enne Hekma <ennehekma@gmail.com>
2016-03-01 11:04:55 +0100

    Add sh script for easy processing

---
+++ python/plot_oscilating_elements.sh
@@ -0,0 +1,6 @@
+#!/bin/bash          
+python plot_oscilating_elements.py ../config/python_plot_oscilating_elements/plot_oscilating_elementsnapa1.json
+python plot_oscilating_elements.py ../config/python_plot_oscilating_elements/plot_oscilating_elementsnapa2.json
+python plot_oscilating_elements.py ../config/python_plot_oscilating_elements/plot_oscilating_elementsnapa3.json
+python plot_oscilating_elements.py ../config/python_plot_oscilating_elements/plot_oscilating_elementsnapa4.json
+python plot_oscilating_elements.py ../config/python_plot_oscilating_elements/plot_oscilating_elementsnapa6.json
