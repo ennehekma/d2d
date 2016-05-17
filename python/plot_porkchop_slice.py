10a6115af72f04226b7fc707ce586d96cef85106
Enne Hekma <ennehekma@gmail.com>
2016-05-17 15:32:53 +0200

    Small changes

--- python/plot_porkchop_slice.py
+++ python/plot_porkchop_slice.py
@@ -126,7 +126,7 @@ cmap = plt.get_cmap('jet')
 fig=plt.figure()
 ax1 = fig.add_subplot(111)
 ax1.scatter(times_of_flight,transfer_delta_vs, color='black')
-ax1.plot(times_of_flight,transfer_delta_vs, color='black')
+# ax1.plot(times_of_flight,transfer_delta_vs, color='black')
 
 formatter = matplotlib.ticker.ScalarFormatter(useOffset=False)
 ax1.xaxis.set_major_formatter(formatter)
@@ -134,15 +134,15 @@ ax1.yaxis.set_major_formatter(formatter)
 plt.ylim([0,math.ceil(transfer_delta_vs.max(0)[0]*1.01)])
 if config['cutoff']!=0:
     plt.ylim(0,config['cutoff'])
-# plt.xlim([-0.001,520000.0001])
+plt.xlim([-0.001,520000.0001])
 ax1.set_xlabel('T$_{ToF}$ [s]', fontsize=13)
 ax1.set_ylabel('Total transfer $\Delta V$ [km/s]', fontsize=13)
-ax2 = ax1.twinx()
-ax2.step(times_of_flight, revolutions,color='r', alpha=0.5)
-ax2.set_ylabel('Amount of revolutions [-]', fontsize=13)
-plt.ylim(0,5)
-plt.title("Porkchop plot of TLE elements " +str(a) + " to " + str(b) + " at departure epoch "  \
-          + str(c) + " [mjd]", fontsize=10, y=1.02)
+# ax2 = ax1.twinx()
+# ax2.step(times_of_flight, revolutions,color='r', alpha=0.5)
+# ax2.set_ylabel('Amount of revolutions [-]', fontsize=13)
+# plt.ylim(0,5)
+# # plt.title("Porkchop plot of TLE elements " +str(a) + " to " + str(b) + " at departure epoch "  \
+#           # + str(c) + " [mjd]", fontsize=10, y=1.02)
 plt.tight_layout()
 
 plt.savefig(config["output_directory"] + "/" + config["scan_figure"] + ".png",                    \
